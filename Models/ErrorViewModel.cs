877640bf0e57926b7e040ee0d33465535435ea9a
Joshua Mellott-Lillie <accu-jmellottlillie@users.noreply.github.com>
2022-01-12 10:27:48 -0500

    Default MVC Project Setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AccuWeather.Web.SkillsTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
